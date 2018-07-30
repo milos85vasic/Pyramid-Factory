b01f40dad5fe568eb1d5f027c376ec67c9bd0179
Milos Vasic <milos85vasic@gmail.com>
2018-07-30 15:00:37 +0200

    Pyramid Factory: Configuration repository.

--- configuration.py
+++ configuration.py
@@ -1,1 +1,3 @@
 from Toolkit.configuration import *
+
+configuration_repository = "https://github.com/milos85vasic/Pyramid-Factory-Config-Default.git"
--- distribute.py
+++ distribute.py
@@ -5,3 +5,11 @@ from Toolkit.commands import *
 from configuration import *
 from Toolkit.system_configuration import *
 
+account = getpass.getuser()
+
+system_configuration = get_system_configuration()
+
+configuration_repo = configuration_repository
+
+incrementPortNumber = True
+
