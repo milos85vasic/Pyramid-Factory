2df3f6c151fae179ed256598b9f9e3fd632ac71e
Milos Vasic <milos85vasic@gmail.com>
2018-08-30 15:57:32 +0200

    Pyramid Factory: Prevent more than 1 service for Pyramid projects.

--- distribute.py
+++ distribute.py
@@ -1,5 +1,6 @@
 import json
 import getpass
+import sys
 
 from Toolkit.commands import *
 from configuration import *
@@ -13,6 +14,14 @@ incrementPortNumber = True
 
 if account in system_configuration:
     if key_services in system_configuration[account]:
+        if key_services in system_configuration[account][key_services]:
+            # TODO: We should do this cehck much earlier!
+            services = system_configuration[account][key_services][key_services]
+            if len(services) > 1:
+                print("No allowed more than one service for Pyramid Factory configuration, we have: " + str(
+                    len(services)))
+                sys.exit(2)
+
         if key_configuration in system_configuration[account][key_services]:
             if key_configuration_repository in system_configuration[account][key_services][key_configuration]:
                 configuration_repo = system_configuration[account][key_services][key_configuration][
@@ -25,7 +34,6 @@ if account in system_configuration:
                         system_configuration[account][key_configuration_port] = system_configuration[account][
                             key_services][key_configuration][key_explicit_port_number]
 
-
 if incrementPortNumber:
     system_configuration[key_configuration_port] = system_configuration[key_configuration_port] + 1
     system_configuration[account][key_configuration_port] = system_configuration[key_configuration_port]
@@ -35,7 +43,7 @@ save_system_configuration(system_configuration)
 steps = [
     python(
         services_distribution_script,
-        configuration_repository
+        configuration_repo
     ),
     curl("http://localhost:" + str(system_configuration[account][key_configuration_port]))
 ]
